950e780d9641110f956d86a86ab5d6948d14c721
Keith Cheung <keith.cheung41@gmail.com>
2020-09-09 15:48:03 -0400

    updated

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#typed', {
+    strings: ["Previously @ LinkedIn, HubSpot (x2), and SMART Technologies"],
+    smartBackspace: true,
+    typeSpeed: 30,
+    backSpeed: 50,
+});
+
